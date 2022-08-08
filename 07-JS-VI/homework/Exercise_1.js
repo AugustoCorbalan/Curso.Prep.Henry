21239f7692b7d39a97351bdc9bba42c8c937e45e
Augusto Corbalan <auguscorbi@gmail.com>
2022-08-08 16:27:04 -0300

    homeworks JSVI

---
+++ 07-JS-VI/homework/Exercise_1.js
@@ -0,0 +1,7 @@
+/*
+Funciones "Callback": Se denomina como "Callback" a las funciones que se pasan como parámetro de otras funciones.
+                      Esto lo podemos hacer ya que las funciones son objetos de primera clase, es decir son del tipo "Object".
+                      Por lo tanto las podemos guardar en variables, pasar como parámetro de otras funciones, creadas dentro de otras funciones,
+                      devueltas desde funciones.
+
+*/
